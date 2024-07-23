c7a624272e435673ed6b77a8d2f4a63139589ea6
Kawan <kawan.wan18@gmail.com>
2024-07-22 22:49:40 -0300

    Autenticação de login com banco de dados e formulários

--- Criptografia/server.js
+++ Criptografia/server.js
@@ -1,24 +1,78 @@
 const express = require('express');
+const path = require('path');
+const mysql = require("mysql2");
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+
+dotenv.config({path: './.env' });
+
 const app = express();
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
+
+db.connect( (error) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("MYSQL Connected...");
+    }
+});
+
+const publicDiretory = path.join(__dirname,'./public');
+app.use(express.static(publicDiretory));
+
+app.use(express.urlencoded({extended: false}));
+
+app.set('view engine', 'hbs');
 
 app.use(express.json());
+app.use(cookieParser());
+
+function verifyJWT(req, res, next) {
+    const token = req.headers['x-access-token'];
+    if (!token) return res.status(401).send('Token não fornecido.');
+
+    jwt.verify(token, process.env.SECRET, (err, decoded) => {
+        if (err) return res.status(401).send('Token inválido.');
+
+        req.userId = decoded.userId;
+        next();
+    });
+}
+
+//Definindo Rotas
+app.use('/', require('./routes/pages'));
+app.use('/auth', require('./routes/auth'));
 
 const users = [];
 
-app.get('/users', (req, res) => {
-    res.json(users)
-});
+// app.get('/users', verifyJWT, (req, res) => {
+//     res.json(users)
+// });
 
-app.post('/users', async (req, res) => {
-    try{
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        const user = { name: req.body.name, password: hashedPassword};
-        users.push(user);
-        res.status(201).send();
-    } catch {
-        res.status(500).send();
-    }
-})
+// app.post('/login', async (req, res) => {
+//     const user = users.find(user => user.name === req.body.name);
+//     if(user == null){
+//         return res.status(400).send('Não foi possível encontrar o usuário!');
+//     }
+
+//     try {
+//         if (await bcrypt.compare(req.body.password, user.password)) {
+//             const token = jwt.sign({userId: user.userId}, process.env.SECRET, { expiresIn: 300 });
+//             res.json({ auth: true, token })
+//         } else {
+//             res.send('Senha incorreta')
+//         }
+//     } catch {
+//         res.status(500).send();
+//     }
+// });
 
 app.listen(3000);
